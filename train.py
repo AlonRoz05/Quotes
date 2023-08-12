0fc37a884b438d4e25da8374f03bc020c12f23f9
AlonRoz <87911114+AlonRoz05@users.noreply.github.com>
2023-08-12 12:49:39 +0300

    Bug fixes and performance improvement

--- train.py
+++ train.py
@@ -1,7 +1,15 @@
 from datasets import load_dataset
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, Seq2SeqTrainingArguments, Seq2SeqTrainer, DataCollatorForSeq2Seq
 
-from helper_fn import compute_metrics
+import nltk
+import evaluate
+import numpy as np
+
+from nltk.tokenize import sent_tokenize
+
+nltk.download("punkt")
+
+metric = evaluate.load("rouge")
 
 model_id = 'google/flan-t5-base'
 
@@ -35,6 +43,35 @@ data_collator = DataCollatorForSeq2Seq(tokenizer,
                                        label_pad_token_id= -100, 
                                        pad_to_multiple_of=8)
 
+# Compute metrics function
+def postprocess_text(preds, labels):
+    preds = [pred.strip() for pred in preds]
+    labels = [label.strip() for label in labels]
+
+    preds = ["\n".join(sent_tokenize(pred)) for pred in preds]
+    labels = ["\n".join(sent_tokenize(label)) for label in labels]
+
+    return preds, labels
+
+def compute_metrics(eval_preds, tokenizer):
+    preds, labels = eval_preds
+    if isinstance(preds, tuple):
+        preds = preds[0]
+
+    decoded_preds = tokenizer.batch_decode(preds, skip_special_tokens=True)
+
+    labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+    decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+
+    decoded_preds, decoded_labels = postprocess_text(decoded_preds, decoded_labels)
+
+    result = metric.compute(predictions=decoded_preds, references=decoded_labels, use_stemmer=True)
+    result = {k: round(v * 100, 4) for k, v in result.items()}
+
+    prediction_lens = [np.count_nonzero(pred != tokenizer.pad_token_id) for pred in preds]
+    result["gen_len"] = np.mean(prediction_lens)
+    return result
+
 # Fine tune the model
 args = Seq2SeqTrainingArguments(
     output_dir="test",
@@ -55,8 +92,8 @@ args = Seq2SeqTrainingArguments(
 trainer = Seq2SeqTrainer(
     model=model,
     args=args,
-    train_dataset=tokenized_train_dataset,
-    eval_dataset=tokenized_test_dataset,
+    train_dataset=tokenized_train_dataset["train"],
+    eval_dataset=tokenized_test_dataset["train"],
     compute_metrics=compute_metrics,
 )
 
