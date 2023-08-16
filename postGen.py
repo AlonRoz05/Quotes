b35c0eef227cafcb5db953dad4ef1bcf6aed2396
AlonRoz <87911114+AlonRoz05@users.noreply.github.com>
2023-08-16 16:26:07 +0300

    Made the script for generating the post

---
+++ postGen.py
@@ -0,0 +1,18 @@
+import textwrap
+from PIL import Image, ImageDraw, ImageFont
+
+def generate_image(text):
+    img = Image.open(f"Imgs/QuoteVibes_Background.png")
+    font = ImageFont.truetype("Fonts/InstagramSansBold.ttf", 55)
+
+    img_width = img.size[0]
+    d = ImageDraw.Draw(img)
+    para = textwrap.wrap(text, width=35)
+
+    text_height, pad = 350, 1
+    for line in para:
+        _, _, w, h = d.textbbox(xy=(0,0), text=line, font=font)
+        d.text(((img_width - w) / 2, text_height), line, font=font, fill=(0, 0, 0))
+        text_height += h + pad
+
+    img.save('post.png')
