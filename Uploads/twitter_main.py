b645eae8c71c13bcc87b31c982fe96e9847b80b4
AlonRoz <87911114+AlonRoz05@users.noreply.github.com>
2023-08-31 15:30:41 +0300

    Update twitter_main.py

--- Uploads/twitter_main.py
+++ Uploads/twitter_main.py
@@ -9,7 +9,7 @@ from transformers import pipeline
 
 from better_profanity import profanity
 
-from twitter_data import bearer_token, API_key, API_secret, access_token, access_token_secret, captions
+from twitter_data import bearer_token, API_key, API_secret, access_token, access_token_secret, captions, hashtags
 
 model_dir = "Rozi05/QuoteVibes_Model_Trained"
 
@@ -24,32 +24,25 @@ api = tweepy.API(auth)
 
 tags_path = "Data/tags.json"
 
-tag_1 = ""
-tag_2 = ""
-tag_3 = ""
-tag_4 = ""
-tag_5 = ""
+tag_1 = random.choice(pd.read_json(tags_path)["tags"])
 
-for i in range(5):
+for i in range(4):
     if i == 0:
-        tag_1 = random.choice(pd.read_json(tags_path)["tags"])
-    
-    elif i == 1:
         tag_2 = random.choice(pd.read_json(tags_path)["tags"])
         while tag_2 == tag_1:
             tag_2 = random.choice(pd.read_json(tags_path)["tags"])
             
-    elif i == 2:
+    elif i == 1:
         tag_3 = random.choice(pd.read_json(tags_path)["tags"])
         while tag_3 == tag_1 and tag_3 == tag_2:
             tag_3 = random.choice(pd.read_json(tags_path)["tags"])
     
-    elif i == 3:
+    elif i == 2:
         tag_4 = random.choice(pd.read_json(tags_path)["tags"])
         while tag_4 == tag_1 and tag_4 == tag_2 and tag_4 == tag_3:
             tag_4 = random.choice(pd.read_json(tags_path)["tags"])
     
-    elif i == 4:
+    elif i == 3:
         tag_5 = random.choice(pd.read_json(tags_path)["tags"])
         while tag_5 == tag_1 and tag_5 == tag_2 and tag_5 == tag_3 and tag_5 == tag_4:
             tag_5 = random.choice(pd.read_json(tags_path)["tags"])
@@ -71,11 +64,20 @@ models_quote = models_quote[0]["generated_text"]
 
 if random.random() <= 0.2:
     postGen.generate_image(models_quote)
-
     media_id = api.media_upload(filename="post.png").media_id_string
-    client.create_tweet(text=random.choice(captions),media_ids=[media_id])
+
+    if random.random() <= 0.5:
+        caption = random.choice(captions)
+        caption = f"{caption} {random.choice(hashtags)}"
+        client.create_tweet(text=caption,media_ids=[media_id])
+        print(f"Succesfuly tweeted an image with this quote: {models_quote} and this caption: {caption}, from this input: {tags}")
+
+    client.create_tweet(media_ids=[media_id])
     print(f"Succesfuly tweeted an image with this quote: {models_quote}, from this input: {tags}")
 
 else:
+    if random.random() <= 0.5:
+        models_quote = f"{models_quote} {random.choice(hashtags)}"
+
     client.create_tweet(text=models_quote)
     print(f"Succesfuly tweeted this quote: {models_quote}, from this input: {tags}")
